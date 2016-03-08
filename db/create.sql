a6483208bb6e8ca516b30384515a8e9917d47085
tehnar <tehnar5@gmail.com>
2016-03-08 23:39:55 +0300

    create script added

---
+++ db/create.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE event_database;
+\connect event_database;
+
+CREATE TABLE IF NOT EXISTS articles (
+    id SERIAL PRIMARY KEY,
+    plain_text TEXT,
+    raw_text TEXT,
+    publish_date TIMESTAMP,
+    url TEXT,
+    annotation TEXT,
+    author TEXT,
+    caption TEXT
+);
+
+CREATE TABLE IF NOT EXISTS entities (
+    id SERIAL PRIMARY KEY,
+    entity_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS actions (
+    id SERIAL PRIMARY KEY,
+    action_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    entity1 INTEGER REFERENCES entities (id), 
+    entity2 INTEGER REFERENCES entities (id),
+    action INTEGER REFERENCES actions (id)
+);
+
+CREATE TABLE IF NOT EXISTS event_sources (
+    source_id INTEGER REFERENCES articles (id),
+    action_id INTEGER REFERENCES actions (id)
+);
