9328e4584ae32934456509b3597380353386e455
tehnar <tehnar5@gmail.com>
2016-04-05 12:59:09 +0300

    Added bootstrap & improved forms a bit

--- web_ui/__init__.py
+++ web_ui/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+Bootstrap(app)
 app.config.from_object('ui_config')
 from web_ui import views
--- web_ui/views.py
+++ web_ui/views.py
@@ -1,24 +1,47 @@
+from flask import redirect, url_for
 from flask import render_template
 from web_ui import app
 from flask.ext.wtf import Form
-from wtforms import IntegerField
-from wtforms.fields.html5 import DateField
+from wtforms import IntegerField, DateField
 from wtforms.validators import DataRequired
-
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 import datetime
+
+nav = Nav()
+
+
+@nav.navigation()
+def main_navbar():
+    return Navbar(
+        'ExTractors',
+        View('Events', 'events'),
+        View('Articles', 'articles'),
+    )
+
+nav.init_app(app)
 
 
 class GetArticlesForm(Form):
     article_count = IntegerField('Max articles per page', validators=[DataRequired()])
-    start_date = DateField('Latest date of article', validators=[DataRequired()])
+    start_date = DateField('Latest date of article', validators=[DataRequired()], format='%d.%m.%y')
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-def index(count=10, date=datetime.datetime.now(), can_submit=True):
+@app.route('/')
+def redirect_to_events():
+    return redirect(url_for('events'))
+
+@app.route('/events', methods=['GET', 'POST'])
+def events(count=10, date=datetime.datetime.now(), can_submit=True):
     form = GetArticlesForm()
+    print(form.validate_on_submit(), form.errors)
     if form.validate_on_submit() and can_submit:
-        return index(form.article_count.data, form.start_date.data, False)
-    events = app.db_handler.get_events_starting_from(count, date)
-    return render_template("index.html", form=form, events=events)
+        print(123)
+        return events(form.article_count.data, form.start_date.data, False)
+    db_events = app.db_handler.get_events_starting_from(count, date)
+    return render_template("index.html", form=form, events=db_events)
 
+
+@app.route('/articles')
+def articles():
+    return "test"
