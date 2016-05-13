1f0c2e4a300a67238b8174de7f8974d74aaeef0e
Seva <tehnar5@gmail.com>
2016-05-13 16:53:06 +0300

    Fixed parsing JetBrains blog (buggy newspaper)

--- data_mining/blogs_parser.py
+++ data_mining/blogs_parser.py
@@ -1,7 +1,10 @@
+import requests
 from data_mining.article import Article as DataMiningArticle
+from newspaper import Article as NewspaperArticle
 import os
 import newspaper
 import newspaper.settings
+from bs4 import BeautifulSoup
 
 
 class BlogsDownloader:
@@ -11,12 +14,24 @@ class BlogsDownloader:
             url = url.strip()
             for file in os.listdir(newspaper.settings.ANCHOR_DIRECTORY):  # clearing newspaper categories cache
                 os.unlink(os.path.join(newspaper.settings.ANCHOR_DIRECTORY, file))
-            articles = newspaper.build(url)
-            for article in articles.articles:
+            articles = newspaper.build(url).articles
+            if url.split('.')[1] == 'jetbrains':  # at least for now. Newspaper is a bit buggy on JetBrains site
+                articles = []
+                for page in range(10):
+                    soup = BeautifulSoup(requests.get(url + '/page/' + str(page)).content, 'html.parser')
+                    for title in soup.find_all('h2', {'class': 'entry-title'}):
+                        articles.append(NewspaperArticle(title.find('a').get('href')))
+            for article in articles:
                 article.download()
                 if not article.is_downloaded:
                     print("Failed to download article:", article.url)
                     continue
                 article.parse()
+                publish_date = article.publish_date
+                if publish_date is None and url.split('.')[1] == 'jetbrains':
+                    soup = BeautifulSoup(requests.get(article.url).content, 'html.parser')
+                    publish_date = soup.find('span', {'class': 'entry-date'}).getText()
+                    # actually, newspaper is very buggy on JetBrains blog and often cannot parse publish date
+                print(publish_date)
                 yield DataMiningArticle(article.html, article.title, article.summary, article.text,
-                                        "", article.canonical_link, "", article.publish_date)
+                                        "", article.canonical_link, "", publish_date)
