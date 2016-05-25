7c704505187764e3bdb5fd082b57c2aeef5eb577
Seva <tehnar5@gmail.com>
2016-05-25 16:25:31 +0300

    Small bugfix

--- web_ui/__init__.py
+++ web_ui/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from web_ui import views
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config.from_object('ui_config')
+
+from web_ui import views
